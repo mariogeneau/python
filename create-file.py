a1e3ac96b9edf208efbaf006f497385f053e384a
mariogeneau <mario.geneau@gmail.com>
2018-10-09 12:37:08 -0400

    v8

--- create-file.py
+++ create-file.py
@@ -1,32 +1,32 @@
+# ¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
 def start_setup():
-    # raw_input returns the empty string for "enter"
+    # ¬¬¬¬¬¬¬¬¬¬¬
     yes = set(['yes','y', 'ye', ''])
     no = set(['no','n'])
-    prompt = '> '
-
-    print ("Hi, welcome to Text File Creator 2000")
-    print ("To get started, give a name to your file (without extension)")
-
+    prompt = '>>> '
+    # ¬¬¬¬¬¬¬¬¬¬¬
+    print("MAD PYTHON TEXT FILE CREATOR")
+    print("To get started, give a name to your file without extension...")
+    # ¬¬¬¬¬¬¬¬¬¬¬
     filename = input(prompt)
-
-    print ("Awesome, now provide the content of the file !)")
-
+    # ¬¬¬¬¬¬¬¬¬¬¬
+    print("Now provide the content of the file...")
+    # ¬¬¬¬¬¬¬¬¬¬¬
     content = input(prompt)
-
-    confirm = input("> Do you want to continue with the creation of " + filename + ".txt ? (Yes/No)").lower()
-
+    # ¬¬¬¬¬¬¬¬¬¬¬
+    confirm = input(">>> Do you want to continue with the creation of " + filename + ".txt? (Yes/No)").lower()
+    # ¬¬¬¬¬¬¬¬¬¬¬
     if confirm in yes:
         text_file = open(filename + ".txt", "w")
         text_file.write(content)
         text_file.close()
-        print ("File succesfully created, come back soon !")
+        print("File succesfully created!")
         return True
     elif confirm in no:
-        print ("Ok, nothing will happen !")
+        print("Ok, nothing will happen!")
         return False
     else:
-       print ("Please answer yes or no !, the setup will start again")
-       # start again
+       print("Please answer yes or no!, the setup will start again")
        start_setup()
-
+# ¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
 start_setup()
