86a47ad4d1a7637a7b883eb8793981bb75b39384
mariogeneau <mario.geneau@gmail.com>
2018-10-11 17:33:20 -0400

    v3

--- buttons.py
+++ buttons.py
@@ -4,18 +4,22 @@ from tkinter import *
 class Window(Frame):
     # ¬¬¬¬¬¬¬¬¬¬¬¬
     def __init__(self, master=None):
-        Frame.__init__(self, master)                 
+        Frame.__init__(self, master)
         self.master = master
         self.init_window()
     # ¬¬¬¬¬¬¬¬¬¬¬¬
-    def init_window(self):  
+    def init_window(self):
         self.master.title("GUI")
         self.pack(fill=BOTH, expand=1)
         quitButton = Button(self, text="Quit")
         quitButton.place(x=10, y=10)
+        quitButton.bind('<Button-1>', self.quit)
     # ¬¬¬¬¬¬¬¬¬¬¬¬
+    def quit(self, event):
+        print("Cool")
+        import sys; sys.exit()
 # ¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
 root = Tk()
-root.geometry("600x600")
+root.geometry("400x400")
 app = Window(root)
-root.mainloop()  
+root.mainloop()
