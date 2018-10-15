7312ddb5327474b9013a8ec53365709301729c47
mariogeneau <mario.geneau@gmail.com>
2018-10-15 11:52:33 -0400

    v1

--- first_app.py
+++ first_app.py
@@ -1,6 +1,7 @@
 # ¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
 from tkinter import *
 from tkinter import messagebox
+# This is cool
 # ¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
 class Window(Frame):
     # ¬¬¬¬¬¬¬¬¬¬¬¬
