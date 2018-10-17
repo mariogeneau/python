4cef20368021ed89cafcd0bb414c0dfa10a7a92c
mariogeneau <mario.geneau@gmail.com>
2018-10-17 14:13:55 -0400

    v4

--- first_app.py
+++ first_app.py
@@ -1,6 +1,8 @@
 # ¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
 from tkinter import *
 from tkinter import messagebox
+import tkinter as tk
+import tkinter.scrolledtext as tkst
 # ¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
 class Window(Frame):
     # ¬¬¬¬¬¬¬¬¬¬¬¬
@@ -9,31 +11,40 @@ class Window(Frame):
         self.master = master
         self.field_file_name = Entry(self, bg="yellow", width=40)
         self.field_file_content = Entry(self, bg="yellow", width=40)
+        self.scroll = tkst.ScrolledText(master=self, wrap=tk.WORD, width=40, height=5, bg="yellow")
         self.init_window()
     # ¬¬¬¬¬¬¬¬¬¬¬¬
     def init_window(self):
         self.master.title("CREATE TEXT FILE")
         self.pack(fill=BOTH, expand=1)
         self.setUpInterface()
+        self.readFile()
+    # ¬¬¬¬¬¬¬¬¬¬¬¬
+    def readFile(self):
+        with open("mario.txt", "r") as f:
+            self.scroll.insert(tk.INSERT, f.read())
     # ¬¬¬¬¬¬¬¬¬¬¬¬
     def setUpInterface(self):
         label_1 = Label(self, text="File Name Without Extension :")
-        label_1.place(x=10, y=20)
+        label_1.pack(side=TOP, pady=10)
         # ---
-        self.field_file_name.place(x=10, y=50)
+        self.field_file_name.pack(side=TOP)
         # ---
         label_2 = Label(self, text="File Content :")
-        label_2.place(x=10, y=100)
+        label_2.pack(side=TOP, pady=10)
         # ---
-        self.field_file_content.place(x=10, y=130)
+        self.field_file_content.pack(side=TOP)
         # ---
         button_create_file = Button(self, text="Create File", highlightbackground ="#8EF0F7", pady=10, padx=10, relief=FLAT)
-        button_create_file.place(x=10, y=180)
+        button_create_file.pack(side=TOP, pady=10)
         button_create_file.bind('<Button-1>', self.createFile)
+        # ---
+        self.scroll.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
     # ¬¬¬¬¬¬¬¬¬¬¬¬
     def createFile(self, event):
         text_file = open(self.field_file_name.get() + ".txt", "w")
-        text_file.write(self.field_file_content.get())
+        self.scroll.insert(tk.INSERT, self.field_file_content.get())
+        text_file.write(self.scroll.get('1.0', 'end-1c'))
         text_file.close()
         self.messages("File Creation Successful")
         self.eraseFields()
---
+++ scrolled_text.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+import tkinter.scrolledtext as tkst
+
+win = tk.Tk()
+frame1 = tk.Frame(
+    master = win,
+    bg = '#808000'
+)
+frame1.pack(fill='both', expand='yes')
+editArea = tkst.ScrolledText(
+    master = frame1,
+    wrap   = tk.WORD,
+    width  = 20,
+    height = 10
+)
+# Don't use widget.place(), use pack or grid instead, since
+# They behave better on scaling the window -- and you don't
+# have to calculate it manually!
+editArea.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
+# Adding some text, to see if scroll is working as we expect it
+editArea.insert(tk.INSERT,
+"""\
+Integer posuere erat a ante venenatis dapibus.
+Posuere velit aliquet.
+Aenean eu leo quam. Pellentesque ornare sem.
+Lacinia quam venenatis vestibulum.
+Nulla vitae elit libero, a pharetra augue.
+Cum sociis natoque penatibus et magnis dis.
+Parturient montes, nascetur ridiculus mus.
+""")
+win.mainloop()
